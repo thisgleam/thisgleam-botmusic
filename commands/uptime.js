871b6dcae8be75645dddc258054e3d13c6387a7b
thisgleam <73107276+thisgleam@users.noreply.github.com>
2021-09-05 11:45:52 +0700

    embed uptime and link uptime

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,4 +1,5 @@
 const { LOCALE } = require("../util/Util");
+const { MessageEmbed } = require("discord.js");
 const i18n = require("i18n");
 
 i18n.setLocale(LOCALE);
@@ -13,12 +14,22 @@ module.exports = {
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
 
+    let uptimeEmbed = new MessageEmbed()
+    .setTitle(`${message.author.tag} Uptime`)
+    .setURL(`https://stats.uptimerobot.com/jLMx4UjqKN`)
+    // .setDescription(`**Uptime**: ${days} day(s), ${hours} hours, ${minutes} minutes, ${seconds} seconds`)
+    .setDescription(`**Uptime**: ${days} days, ${hours} hours`)
+    .setFooter('Design : thisgleam', 'https://cdn.discordapp.com/avatars/849261647859417118/bc4c4ddf312dd058c1d2e5bd826f69b4.png?size=2048')
+    .setTimestamp()
+    .setColor("#00BCFF");
+
     seconds %= 60;
     minutes %= 60;
     hours %= 24;
 
     return message
-      .reply(i18n.__mf('uptime.result', {days: days, hours: hours, minutes: minutes, seconds: seconds}))
+      // .reply(i18n.__mf('uptime.result', {days: days, hours: hours, minutes: minutes, seconds: seconds}))
+      .reply(uptimeEmbed)
       .catch(console.error);
   }
 };
